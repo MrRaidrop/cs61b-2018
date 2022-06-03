c23f13e70c04a3caba8316c181f21a28c2788fdc
GreysonYu <yzhdsg1230@hotmail.com>
2022-06-03 23:21:49 +0800

    stop the messing

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,4 +1,169 @@
 public class ArrayDeque<T> {
+        /** array to save data.*/
+        private T[] array;
+        /** size of the deque. */
+        private int size;
+
+        /** size of the array. */
+        private int length;
+
+        /** front index. */
+        private int front;
+
+        /** last index. */
+        private int last;
+
+        /** constructor for ArrayDeque. */
+        public ArrayDeque() {
+            array = (T[]) new Object[8];
+            size = 0;
+            length = 8;
+            front = 4;
+            last = 4;
+        }
+
+        /** decide if the deque is empty.
+         * @return true if the deque is empty, vice versa.
+         */
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        /** return the size of the deque. */
+        public int size() {
+            return size;
+        }
+
+        /** return the "index - 1".
+         * @param index index
+         */
+        private int minusOne(int index) {
+            if (index == 0) {
+                return length - 1;
+            }
+            return index - 1;
+        }
+
+        /** return the "index + 1".
+         * @param index index
+         */
+        private int plusOne(int index, int module) {
+            index %= module;
+            if (index == module - 1) {
+                return 0;
+            }
+            return index + 1;
+        }
+
+        private void grow() {
+            T[] newArray = (T[]) new Object[length * 2];
+            int ptr1 = front;
+            int ptr2 = length;
+            while (ptr1 != last) {
+                newArray[ptr2] = array[ptr1];
+                ptr1 = plusOne(ptr1, length);
+                ptr2 = plusOne(ptr2, length * 2);
+            }
+            front = length;
+            last = ptr2;
+            array = newArray;
+            length *= 2;
+        }
+
+        private void shrink() {
+            T[] newArray = (T[]) new Object[length / 2];
+            int ptr1 = front;
+            int ptr2 = length / 4;
+            while (ptr1 != last) {
+                newArray[ptr2] = array[ptr1];
+                ptr1 = plusOne(ptr1, length);
+                ptr2 = plusOne(ptr2, length / 2);
+            }
+            front = length / 4;
+            last = ptr2;
+            array = newArray;
+            length /= 2;
+        }
+
+        /** add one item at the front of the deque.
+         * @param item the item we want to add
+         */
+        public void addFirst(T item) {
+            if (size == length - 1) {
+                grow();
+            }
+            front = minusOne(front);
+            array[front] = item;
+            size++;
+        }
+
+        /** add one item at the end of the deque.
+         * @param item item we want to add
+         */
+        public void addLast(T item) {
+            if (size == length - 1) {
+                grow();
+            }
+            array[last] = item;
+            last = plusOne(last, length);
+            size++;
+        }
+
+        /** remove the first item.
+         * @return the removed first item
+         */
+        public T removeFirst() {
+            if (length >= 16 && length / size >= 4) {
+                shrink();
+            }
+            if (size == 0) {
+                return null;
+            }
+            T ret = array[front];
+            front = plusOne(front, length);
+            size--;
+            return ret;
+        }
+
+        /** remove the last item.
+         * @return the removed last item
+         */
+        public T removeLast() {
+            if (length >= 16 && length / size >= 4) {
+                shrink();
+            }
+            if (size == 0) {
+                return null;
+            }
+            last = minusOne(last);
+            size--;
+            return array[last];
+        }
+
+        /** return the item indexed at index.
+         * @param index index
+         */
+        public T get(int index) {
+            if (index >= size) {
+                return null;
+            }
+            int ptr = front;
+            for (int i = 0; i < index; i++) {
+                ptr = plusOne(ptr, length);
+            }
+            return array[ptr];
+        }
+
+        /** print the entire deque from front to end. */
+        public void printDeque() {
+            int ptr = front;
+            while (ptr != last) {
+                System.out.print(array[ptr] + " ");
+                ptr = plusOne(ptr, length);
+            }
+        }
+
+    /**
     private T[] items;
     //how many items in items
     private int size;
@@ -7,7 +172,7 @@ public class ArrayDeque<T> {
 
     /**
      * Creates an empty list.
-     */
+
     public ArrayDeque() {
         items = (T[]) new Object[8];
         size = 0;
@@ -23,7 +188,7 @@ public class ArrayDeque<T> {
      * Resizes the underlying array to the target capacity.
      */
 
-    // 扩容为原来的2倍
+    /* 扩容为原来的2倍
     private void doubleCapacity() {
         assert head == tail;
         int p = head;
@@ -43,7 +208,7 @@ public class ArrayDeque<T> {
 
     /**
      * add a T item to the first of the Deque and return nothing.
-     */
+
     public void addFirst(T t) {
         if (t == null)
             throw new NullPointerException();
@@ -57,7 +222,7 @@ public class ArrayDeque<T> {
 
     /**
      * add a T item to the last of the Deque and return nothing.
-     */
+
     public void addLast(T t) {
         if (t == null)
             throw new NullPointerException();
@@ -71,14 +236,14 @@ public class ArrayDeque<T> {
 
     /**
      * Returns true if deque is empty, false otherwise.
-     */
+
     public boolean isEmpty() {
         return head == tail;
     }
 
     /**
      * returns the number of items in the deque
-     */
+
     public int size() {
         return (tail - head) & (items.length - 1);
     }
@@ -123,7 +288,7 @@ public class ArrayDeque<T> {
      * Prints the items in the deque from first to last,
      * separated by a space. Once all the items have been printed,
      * print out a new line.
-     */
+
     public void printDeque() {
         for (int i = 0; i < size; i += 1) {
             System.out.println(items[i] + " ");
@@ -133,19 +298,21 @@ public class ArrayDeque<T> {
 
     /**
      * Gets the index th item in the list (0 is the front).
-     */
+
     public T get(int index) {
         if (index < 0 || index >= size) {
             return null;
         }
         return (T) items[index];
     }
+
     private T getFirst() {
         T x = items[head];
         if (x == null)
             return null;
         return x;
     }
+
     private T getLast() {
         // 处理临界情况（当tail为0时），与后的结果为elements.length - 1。
         T x = items[(tail - 1) & (items.length - 1)];
@@ -153,9 +320,9 @@ public class ArrayDeque<T> {
             return null;
         return x;
     }
+//this is for fun
 
-
+*/
 }
-
 
 
