f4dcc741787f3eb58b6bd6f608e945381f391233
GreysonYu <yzhdsg1230@hotmail.com>
2022-08-19 15:24:30 +0800

    lab 13 from 2018

--- lab13/CountingSort.java
+++ lab13/CountingSort.java
@@ -54,7 +54,7 @@ public class CountingSort {
         }
 
         // return the sorted array
-        return sorted;
+        return sorted2;
     }
 
     /**
@@ -66,7 +66,97 @@ public class CountingSort {
      * @param arr int array that will be sorted
      */
     public static int[] betterCountingSort(int[] arr) {
-        // TODO make counting sort work with arrays containing negative numbers.
-        return null;
+        // find max & min
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int pos0Count = 0;
+        int negeCount = 0;
+        for (int i : arr) {
+            max = max > i ? max : i;
+            if (i >= 0) {
+                pos0Count++;
+            } else {
+                negeCount++;
+            }
+            min = min < i ? min : i;
+        }
+        int[] counts;
+        if (min < 0) {
+            // gather all the counts for each value
+            counts = new int[max + 1];
+            int[] counts2 = new int[Math.abs(min) + 1];
+            for (int i : arr) {
+                if (i < 0) {
+                    counts2[Math.abs(i)]++;
+                } else {
+                    counts[i]++;
+                }
+            }
+            int[] starts = new int[max + 1];
+            int[] starts2 = new int[Math.abs(min) + 1];
+            int pos = 0;
+            for (int i = 0; i < starts.length; i += 1) {
+                starts[i] = pos;
+                pos += counts[i];
+            }
+            pos = 0;
+            for (int i = 0; i < starts2.length; i += 1) {
+                starts2[i] = pos;
+                pos += counts2[i];
+            }
+
+            int[] sorted1 = new int[pos0Count];
+            int[] sorted2 = new int[negeCount];
+            int[] sorted = new int[arr.length];
+            for (int i = 0; i < arr.length; i += 1) {
+                int item = arr[i];
+                if (item < 0) {
+                    int place = starts2[Math.abs(item)];
+                    sorted2[place] = item;
+                    starts2[Math.abs(item)]++;
+                } else {
+                    int place = starts[item];
+                    sorted1[place] = item;
+                    starts[item] += 1;
+                }
+            }
+            for (int i = negeCount - 1, j = 0; i > 0; i--, j++) {
+                sorted[j] = sorted2[i];
+            }
+            for (int i = 0, j = negeCount; j < arr.length; i++, j++) {
+                sorted[j] = sorted1[i];
+            }
+
+            // return the sorted array
+            return sorted;
+
+
+
+        } else {
+            counts = new int[max + 1];
+            for (int i : arr) {
+                counts[i]++;
+            }
+
+            // however, below is a more proper, generalized implementation of
+            // counting sort that uses start position calculation
+            int[] starts = new int[max + 1];
+            int pos = 0;
+            for (int i = 0; i < starts.length; i += 1) {
+                starts[i] = pos;
+                pos += counts[i];
+            }
+
+            int[] sorted2 = new int[arr.length];
+            for (int i = 0; i < arr.length; i += 1) {
+                int item = arr[i];
+                int place = starts[item];
+                sorted2[place] = item;
+                starts[item] += 1;
+            }
+
+            // return the sorted array
+            return sorted2;
+        }
     }
 }
--- lab13/RadixSort.java
+++ lab13/RadixSort.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * Class for doing Radix sort
  *
@@ -16,8 +21,17 @@ public class RadixSort {
      * @return String[] the sorted array
      */
     public static String[] sort(String[] asciis) {
-        // TODO: Implement LSD Sort
-        return null;
+        int max = Integer.MIN_VALUE;
+        for (String s : asciis) {
+            if (s.length() > max) {
+                max = s.length();
+            }
+        }
+        String[] res = asciis.clone();
+        for (int i = max - 1; i >= 0; i--) {
+            sortHelperLSD(res, i);
+        }
+        return res;
     }
 
     /**
@@ -27,9 +41,62 @@ public class RadixSort {
      * @param index The position to sort the Strings on.
      */
     private static void sortHelperLSD(String[] asciis, int index) {
-        // Optional LSD helper method for required LSD radix sort
-        return;
+        int base = 256;
+        int[] counts = new int[base + 1];
+        for (String s : asciis) {
+            int cur = index >= s.length() ? 0 : s.charAt(index) + 1;
+            counts[cur]++;
+        }
+        int[] starts = new int[base + 1];
+        int pos = 0;
+        for (int i = 0; i < starts.length; i += 1) {
+            starts[i] = pos;
+            pos += counts[i];
+        }
+        String[] sorted = new String[asciis.length];
+        for (int i = 0; i < asciis.length; i += 1) {
+            String s = asciis[i];
+            int c = index >= s.length() ? 0 : s.charAt(index) + 1;
+            int place = starts[c];
+            sorted[place] = s;
+            starts[c]++;
+        }
+
+        for (int i = 0; i < asciis.length; i += 1) {
+            asciis[i] = sorted[i];
+        }
     }
+
+    public static long StringtoLong(String s) {
+        long res = 0;
+        for (int i = s.length() - 1, j = 0; i >= 0; i--, j++) {
+            res += s.charAt(i) * Math.pow(256, j);
+        }
+        return res;
+    }
+    public static String LongtoString(long a) {
+        Stack<Character> chars= new Stack<>();
+        while(a > 1) {
+            char c = (char) (a % 256);
+            chars.push(c);
+            a = a / 256;
+        }
+        String res = "";
+        while(!chars.empty()) {
+            res += chars.pop();
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        String[] strings = new String[] {"abfc", "bafaa", "cbafafa"};
+        String[] res = sort(strings);
+        for (String s : res) {
+            System.out.println(s);
+        }
+    }
+
 
     /**
      * MSD radix sort helper function that recursively calls itself to achieve the sorted array.
